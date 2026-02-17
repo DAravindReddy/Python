10069fe8377fe67f13c0b104fe5fd965767fdc13
Aravind Reddy <aravindreddy0726@gmail.com>
2026-02-17 16:04:58 +0530

    adding files

---
+++ templates/template3.py
@@ -0,0 +1,11 @@
+
+a = 10
+b = 5
+print("Addition: ", a+b) #Addition
+print("Subraction: ", a-b) #Subraction
+print("Multiplication: ", a*b) #Multiplication
+print("Division: ", a/b) #Division
+print("Modulus: ", a%b) #Modulus
+print("Floor division: ", a // b) #Floordivision
+print("Power: ",a ** b) #power
+
---
+++ templates/template5.py
@@ -0,0 +1,7 @@
+
+x = 10
+y = 20
+print(x > 5 and y > 10)
+print(x > 5 or y < 10)
+print(not(x > 5))
+
